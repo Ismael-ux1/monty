1ec25e2887a3b50efaf43a047eb945b3b88f4bae
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2024-05-04 13:35:00 -0700

    header file

--- monty.h
+++ monty.h
@@ -1,15 +1,6 @@
 #ifndef MONTY_H
 #define MONTY_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stddef.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/syscall.h>
-
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -21,23 +12,25 @@
  */
 typedef struct stack_s
 {
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
 } stack_t;
 
-int _strcmp(char *opcode, char *list);
-int parse_line(char *line, int line_number, stack_t **stack);
-void free_stack(stack_t **stack);
-int execute_opcode(char *opcode, char *argument, unsigned int line_number, stack_t **stack);
-void push(stack_t **stack, unsigned int line_number, int push_value);
-void pall(stack_t **stack, unsigned int line_number);
-void pint(stack_t **stack, unsigned int line_number);
-void pop(stack_t **stack, unsigned int line_number);
-void swap(stack_t **stack, unsigned int line_number);
-stack_t *add_node(stack_t **stack, int n);
-void execute(char *string[], stack_t *stack, int num_lines);
-int nlfind(char *list);
-void free_list(char *a[]);
-int combfind(char *list, int ln);
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+
 #endif /* MONTY_H */
