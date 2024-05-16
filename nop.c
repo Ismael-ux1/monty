ed8449e7e3c1c8ce36b4b5b2763e8504b0f51465
Ismael-ux1 <ismaelabdulahi1@gmail.com>
2024-05-16 13:47:41 -0700

    nop

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * nop - doesn't do anything.
+ * @stack: double pointer to the head of the stack.
+ * @line_number: line number of the opcode in the file.
+ *
+ * Return: void
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
